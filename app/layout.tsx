c463a4ccf1c21bac1a2b1d0714c92ab49c47970e
iotatheta09 <iota.theta09@gmail.com>
2026-06-15 22:05:19 +0530

    authentication added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/header";
 import Footer from "@/components/common/footer";
+import { ClerkProvider } from "@clerk/nextjs";
 const fontSans = FontSans({
   subsets:['latin'],
   variable:'--font-sans',
@@ -23,11 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html
       lang="en" 
       
     >
       <body className={`${fontSans.variable} font-sans antialiased`}>
+         
         <div className="relative flex min-h-screen flex-col">
 
         
@@ -37,7 +40,9 @@ export default function RootLayout({
         </main>
         <Footer />
         </div>
+       
       </body>
     </html>
+    </ClerkProvider>
   );
 }
